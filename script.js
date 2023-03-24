8d5d367a7a175a764bf6d55470287d19719ac03d
Pratham Garg <aggarwal.prathamgarg@gmail.com>
2023-03-24 20:43:17 +0530

    Updated Whiteboard

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
-const socket = io("https://whiteboard-server.onrender.com");
-socket.on("connect", () => {
-    console.log("connected");
-});
-socket.on("newData", ({ prevX, prevY, x, y, color, width }) => {
-    ctx.beginPath();
-    ctx.strokeStyle = color;
-    ctx.lineWidth = width;
-    ctx.moveTo(prevX, prevY)
-    ctx.lineTo(x, y);
-    ctx.stroke();
-});
+//eslint-disable-next-line no-undef
+// const socket = io("localhost:3001");
+// socket.on("connect", () => {
+//     console.log("connected");
+// });
 
 
-const canvas = document.querySelector('canvas');
+
+
+
+const canvas = document.querySelector(' canvas');
 const ctx = canvas.getContext('2d');
-const cursor = document.querySelector('#cursor');
 const width = canvas.width = window.innerWidth;
-const height = canvas.height = window.innerHeight - 100;
+const height = canvas.height = window.innerHeight - 80;
 let time = performance.now();
+const cursor = document.querySelector("#cursor");
+const shapeDemo = document.querySelector("#shape-demo");
+ctx.lineJoin = 'round';
+
 
 let interval = 0;
 let colorInterval = 0;
@@ -25,19 +24,17 @@ let prevX = 0;
 let prevY = 0;
 let x = 0;
 let y = 0;
-let elapsed = performance.now();
 const triangle = [{ x: 0, y: 0 }, { x: 0, y: 0 }, { x: 0, y: 0 }];
 const rectangle = [0, 0, 0, 0];
 const circle = [0, 0, 0, 0, 0];
 
 
-
-const data = { color: "#000", width: 1 };
+const whiteBoard = document.body;
+const data = { color: "black", thickness: 1 };
 const colors = document.querySelectorAll(".colors .color");
 const sizes = document.querySelectorAll(".sizes .size");
 const eraser = document.querySelector("#eraser");
 const shapes = document.querySelectorAll(".shapes .shape");
-
 // click events
 
 colors.forEach((color, index) => {
@@ -45,14 +42,16 @@ colors.forEach((color, index) => {
         clearInterval(colorInterval);
         colors.forEach(color => color.classList.remove("active"));
         color.classList.add("active");
+        sizes[data.thickness - 1].classList.add("active");
         eraser.classList.remove("active");
         if (index == colors.length - 1) {
             changeContinuous();
         } else {
             data.color = getComputedStyle(color).getPropertyValue("--color");
             ctx.strokeStyle = data.color;
-            ctx.lineWidth = data.width;
+            ctx.lineWidth = data.thickness;
         }
+        changeColorShape();
     })
 })
 
@@ -61,8 +60,13 @@ sizes.forEach(size => {
         sizes.forEach(size => size.classList.remove("active"));
         size.classList.add("active");
         eraser.classList.remove("active");
-        data.width = getComputedStyle(size).getPropertyValue("--width");
-        ctx.lineWidth = data.width;
+        colors.forEach(color => {
+            if (getComputedStyle(color).getPropertyValue("--color") == data.color) {
+                color.classList.add("active");
+            }
+        });
+        data.thickness = getComputedStyle(size).getPropertyValue("--width");
+        ctx.lineWidth = data.thickness;
         ctx.strokeStyle = data.color;
     })
 })
@@ -82,8 +86,6 @@ clearScreenBtn.addEventListener("click", () => {
 eraser.addEventListener("click", () => {
     ctx.strokeStyle = "#fff";
     ctx.lineWidth = 20;
-    data.color = "#fff";
-    data.width = 20;
     eraser.classList.add("active");
     sizes.forEach(size => size.classList.remove("active"));
     colors.forEach(color => color.classList.remove("active"));
@@ -95,10 +97,19 @@ eraser.addEventListener("click", () => {
 const pen = document.querySelector("#pen");
 
 pen.addEventListener("click", () => {
+    ctx.lineWidth = data.thickness;
+    ctx.strokeStyle = data.color;
     shapes.forEach(shape => shape.classList.remove("active"));
     eraser.classList.remove("active");
     clearInterval(colorInterval);
     pen.classList.add("active")
+    changeColorShape();
+    sizes[data.thickness - 1].classList.add("active");
+    colors.forEach(color => {
+        if (getComputedStyle(color).getPropertyValue("--color") == data.color) {
+            color.classList.add("active");
+        }
+    });
 })
 
 shapes.forEach(shape => {
@@ -107,15 +118,54 @@ shapes.forEach(shape => {
         pen.classList.remove("active");
         shape.classList.add("active");
         eraser.classList.remove("active");
+        ctx.lineWidth = data.thickness;
+        ctx.strokeStyle = data.color;
+        sizes[data.thickness - 1].classList.add("active");
+        colors.forEach(color => {
+            if (getComputedStyle(color).getPropertyValue("--color") == data.color) {
+                color.classList.add("active");
+            }
+        });
         clearInterval(colorInterval);
+        changeColorShape();
     })
 })
 
+function changeColorShape() {
+    const shapes = document.querySelectorAll(".shapes .shape, .shapes #pen");
+    shapes.forEach(shape => {
+        if (shape.classList.contains("active")) {
+            shape.style.color = data.color;
+            shape.style.borderColor = data.color;
+        } else {
+            shape.style.color = "black";
+            shape.style.borderColor = "black";
+        }
+    })
+}
 
 
 // event listeners
 
 window.addEventListener('mousemove', (e) => {
+    if (e.target != canvas) {
+        cursor.style.display = "none";
+        whiteBoard.style.cursor = "default";
+        return;
+    }
+    cursor.style.display = "block";
+    whiteBoard.style.cursor = "none";
+    if (eraser.classList.contains("active")) {
+        cursor.style.backgroundImage = "url('./eraser.png')"
+    } else if (pen.classList.contains("active")) {
+        cursor.style.backgroundImage = "url('./pencil.png')"
+    } else {
+        whiteBoard.style.cursor = "crosshair";
+        cursor.style.backgroundImage = "none";
+
+    }
+    cursor.style.left = e.clientX + "px";
+    cursor.style.top = e.clientY + "px";
     if (performance.now() - time < 10) return;
     prevX = x;
     prevY = y;
@@ -136,6 +186,7 @@ canvas.addEventListener('mousedown', (e) => {
 
         canvas.addEventListener("mousemove", callDrawShape);
 
+        // eslint-disable-next-line no-inner-declarations
         function callDrawShape(e) {
             drawShape(e, shape);
         }
@@ -153,7 +204,7 @@ canvas.addEventListener('mousedown', (e) => {
 
 
         return;
-    };
+    }
     interval = setInterval(() => drawLine(), 10);
 })
 
@@ -168,60 +219,120 @@ canvas.addEventListener("mouseleave", () => {
 
 function drawLine() {
     ctx.beginPath();
-    ctx.strokeStyle = data.color;
+    ctx.strokeStyle = eraser.classList.contains("active") ? "#fff" : data.color;
     ctx.moveTo(prevX, prevY)
     ctx.lineTo(x, y);
     ctx.stroke();
-    socket.emit("newData", { prevX, prevY, x, y, color: data.color, width: data.width });
+    // socket.emit("drawData", { prevX, prevY, x, y, color: data.color, thickness: data.thickness, shape: eraser.classList.contains("active") ? "eraser" : "pen" });
 }
 
 
 function drawShape(e, shape) {
-    console.log("drawing shape");
+    const toolbar = document.querySelector(".toolbar");
     if (shape.id == "triangle") {
         triangle[0].y = e.offsetY;
         triangle[2].x = e.offsetX;
         triangle[2].y = e.offsetY;
         triangle[1].x = (triangle[0].x + triangle[2].x) / 2;
+        // shapeDemo.classList.add("triangle");
+        // shapeDemo.classList.remove("rectangle");
+        // shapeDemo.style.left = triangle[0].x + "px";
+        // shapeDemo.style.top = triangle[1].y + toolbar.offsetHeight + "px";
+        // shapeDemo.style.width = triangle[2].x - triangle[0].x + "px";
+        // shapeDemo.style.height = triangle[2].y - triangle[1].y + "px";
+
+
     }
 
     if (shape.id == "rectangle") {
         rectangle[2] = e.offsetX;
         rectangle[3] = e.offsetY;
+        shapeDemo.classList.add("rectangle");
+        shapeDemo.classList.remove("triangle");
+        shapeDemo.style.left = rectangle[0] + "px";
+        shapeDemo.style.top = rectangle[1] + toolbar.offsetHeight + "px";
+        shapeDemo.style.width = rectangle[2] - rectangle[0] + "px";
+        shapeDemo.style.height = rectangle[3] - rectangle[1] + "px";
     }
 
     if (shape.id == "circle") {
         circle[2] = (e.offsetX - circle[0]) / 2 + circle[0];
         circle[3] = (e.offsetY - circle[1]) / 2 + circle[1];
         circle[4] = Math.sqrt(Math.pow(e.offsetX - circle[2], 2) + Math.pow(e.offsetY - circle[3], 2));
+        shapeDemo.classList.remove("rectangle", "triangle");
+        // shapeDemo.style.borderRadius = "50%";
+        // shapeDemo.style.left = circle[2] - circle[4] + "px";
+        // shapeDemo.style.top = circle[3] - circle[4] + toolbar.offsetHeight + "px";
+        // shapeDemo.style.width = circle[4] * 2 + "px";
+        // shapeDemo.style.height = circle[4] * 2 + "px"
+
     }
 }
 
-
 function drawTriangle() {
+    ctx.fillStyle = data.color;
+    ctx.lineWidth = data.thickness;
     ctx.beginPath();
     ctx.moveTo(triangle[0].x, triangle[0].y);
     ctx.lineTo(triangle[1].x, triangle[1].y);
     ctx.lineTo(triangle[2].x, triangle[2].y);
     ctx.lineTo(triangle[0].x, triangle[0].y);
     ctx.stroke();
+    shapeDemo.style.width = shapeDemo.style.height = 0;
+    // socket.emit("drawData", { triangle, color: data.color, thickness: data.thickness, shape: "triangle" });
 }
 
 function drawRectangle() {
+    ctx.fillStyle = data.color;
+    ctx.lineWidth = data.thickness;
     ctx.beginPath();
     ctx.rect(rectangle[0], rectangle[1], rectangle[2] - rectangle[0], rectangle[3] - rectangle[1]);
     ctx.stroke();
+    shapeDemo.style.width = shapeDemo.style.height = 0;
+    // socket.emit("drawData", { rectangle, color: data.color, thickness: data.thickness, shape: "rectangle" });
 }
 
 function drawCircle() {
+    ctx.fillStyle = data.color;
+    ctx.lineWidth = data.thickness;
     ctx.beginPath();
     ctx.arc(circle[2], circle[3], circle[4], 0, 2 * Math.PI);
     ctx.stroke();
+    shapeDemo.style.width = shapeDemo.style.height = 0;
+
+    // socket.emit("drawData", { x: circle[2], y: circle[3], radius: circle[4], color: data.color, thickness: data.thickness, shape: "circle" });
 }
 
-
-
-
-
-
-
+// socket.on("drawData", (data) => {
+//     ctx.strokeStyle = data.color;
+//     ctx.lineWidth = data.thickness;
+//     if (data.shape === "triangle") {
+//         ctx.beginPath();
+//         ctx.moveTo(data.triangle[0].x, data.triangle[0].y);
+//         ctx.lineTo(data.triangle[1].x, data.triangle[1].y);
+//         ctx.lineTo(data.triangle[2].x, data.triangle[2].y);
+//         ctx.lineTo(data.triangle[0].x, data.triangle[0].y);
+//         ctx.stroke();
+//         return;
+//     }
+//     if (data.shape === "rectangle") {
+//         ctx.beginPath();
+//         ctx.rect(data.rectangle[0], data.rectangle[1], data.rectangle[2] - data.rectangle[0], data.rectangle[3] - data.rectangle[1]);
+//         ctx.stroke();
+//         return;
+//     }
+//     if (data.shape === "circle") {
+//         ctx.beginPath();
+//         ctx.arc(data.x, data.y, data.radius, 0, 2 * Math.PI);
+//         ctx.stroke();
+//         return;
+//     }
+//     if (data.shape === "eraser") {
+//         ctx.lineWidth = 20;
+//         ctx.strokeStyle = "#fff";
+//     }
+//     ctx.beginPath();
+//     ctx.moveTo(data.prevX, data.prevY)
+//     ctx.lineTo(data.x, data.y);
+//     ctx.stroke();
+// });
